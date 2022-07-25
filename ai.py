f404b7f54d10ea097cd6a7628d3ad707fda48972
Chandra Prakash Jangid <talkme.kumar@gmail.com>
2022-07-25 18:12:38 +0530

    forward method added

--- ai.py
+++ ai.py
@@ -35,5 +35,12 @@ class CNN(nn.Module):
         x = F.relu(F.max_pool2d(self.convolution2(x),3, 2))
         x = F.relu(F.max_pool2d(self.convolution3(x),3, 2))
         return x.data.view(1, -1).size(1)
-        
+    
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.convolution1(x),3,2))
+        x = F.relu(F.max_pool2d(self.convolution2(x),3,2))
+        x = F.relu(F.max_pool2d(self.convolution3(x),3,2))
+        x = x.view(x.size(0),-1)
+        x = F.relu(self.fc1(x))            
+        return x
         
