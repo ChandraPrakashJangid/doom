c1aedf42141838721e3160ef41daa83ac613e8df
Chandra Prakash Jangid <talkme.kumar@gmail.com>
2022-07-29 17:51:41 +0530

    Train method implemented

--- ai.py
+++ ai.py
@@ -108,10 +108,35 @@ class MA:
     def add(self, rewards):
         if isinstance(rewards, list):
             self.list_of_rewards += rewards
-        else 
+        else :
             self.list_of_rewards.append(rewards)
         while len(self.list_of_rewards) > self.size:
-            del self,list_of_rewards[0]
+            del self.list_of_rewards[0]
         
+    def average(self):
+        return np.mean(self.list_of_rewards)
+    
+ma = MA(100)
+
+# Training the AI
+loss = nn.MSELoss()
+optimizer = optim.Adam(cnn.parameters(), lr = 0.001)
+np_epoch = 100
+for epoch in range(1, np_epoch + 1):
+    memory.run_steps(200)    
+    for batch in memory.sample_batch(128):
+        inputs, targets = eligibility_trace(batch)
+        inputs, targets = Variable(inputs), Variable(targets)
+        predictions = cnn(inputs)
+        loss_error = loss(predictions, targets)
+        optimizer.zero_grad()
+        loss_error.backward()
+        optimizer.step()
+    rewards_steps = n_steps.rewards_steps()
+    ma.add(rewards_steps)
+    avg_reward = ma.average()
+    print("Epoch: %s, Average Reward: %s"%(str(epoch), str(avg_reward)))
+
+                       
     
         
