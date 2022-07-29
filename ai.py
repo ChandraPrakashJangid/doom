8f3b1e40d40d29786f6cc8520648806af00b237d
Chandra Prakash Jangid <talkme.kumar@gmail.com>
2022-07-29 17:27:20 +0530

    Eligibility Trace method added

--- ai.py
+++ ai.py
@@ -69,5 +69,33 @@ class AI:
     
 # Getting the Doom environment
 doom_env = image_preprocessing.PreprocessImage(SkipWrapper(4)(ToDiscrete("minimal")(gym.make("ppaquette/DoomCorridor-v0"))),width=80, height = 80, grayscale=True)
-doom_env = gym.wrappers.Monitor(doom_env, "videos", force = True)    
-    
+doom_env = gym.wrappers.Monitor(doom_env, "videos", force = True)  
+number_actions = doom_env.action_space.n
+
+# Building an AI
+cnn = CNN(number_actions)
+softmax_body = SoftmaxBody(T = 1.0)
+ai = AI(brain=cnn, body=softmax_body)
+
+# Setting up Experience Replay
+n_steps = experience_replay.NStepProgress(env = doom_env, ai = ai, n_step = 10)
+memory = experience_replay.ReplayMemory(n_steps = n_steps, capacity=10000)
+
+#Implementing Eligibility Trace
+def eligibility_trace(batch):
+    gamma = 0.99
+    inputs = []
+    targets = []
+    for series in batch:
+        input = Variable(torch.from_numpy(np.array([series[0].state, series[-1].state], dtype = np.float32)))
+        output = cnn(input)
+        cumul_reward = 0.0 if series[-1].done else output[1].data.max()
+        for step in reversed(series[:-1]):
+            cumul_reward = step.reward + gamma* cumul_reward
+        state = series[0].state
+        target = output[0].data
+        target[series[0].action] = cumul_reward
+        inputs.append(state)
+        targets.append(target)
+    return torch.from_numpy(np.array(inputs, dtype = np.float32)),torch.stack(targets)
+        
